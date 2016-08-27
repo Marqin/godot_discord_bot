467b4bbd91adc3c1391a53324db629a9abdcc957
Hubert Jarosz <marqin.pl@gmail.com>
2016-08-27 13:19:53 +0200

    move prefix config to config.exs

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ use Mix.Config
 config :porcelain, goon_warn_if_missing: false
 
 config :godot_discord_bot, token: System.get_env("GODOT_DISCORD_BOT_TOKEN")
+config :godot_discord_bot, cmd_prefix: "!"
 
 # you can also config :token in gitignored "secret.exs"
 # import_config "secret.exs"
--- lib/godot_discord_bot/bot.ex
+++ lib/godot_discord_bot/bot.ex
@@ -3,7 +3,8 @@ defmodule GodotDiscordBot.Bot do
   alias DiscordEx.Client.Helpers.MessageHelper
 
   def handle_message(payload, state) do
-    parsed_cmd = MessageHelper.msg_command_parse(payload, ".")
+    prefix = Application.get_env(:godot_discord_bot, :cmd_prefix)
+    parsed_cmd = MessageHelper.msg_command_parse(payload, prefix)
     GodotDiscordBot.Bot.CommandHandler.handle(parsed_cmd, payload.data, state)
   end
 
